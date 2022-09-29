5dade13573d19e1be59a748ec3971bfd3745830e
alejandro3469 <alejandro.perez3469@gmail.com>
2022-09-28 21:06:53 -0500

    Search by name (try)
    Not working yet. Data updates successfully but the data in the UI doesn't update.
    Is it bc of hydration??

--- pages/components/SearchBar.js
+++ pages/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SearchBar() {
+export default function SearchBar({ onchange }) {
   return (
     <div className="searchbar-container">
       <div className="curve"></div>
--- pages/countries/index.js
+++ pages/countries/index.js
@@ -1,43 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import SearchBar from "../components/SearchBar";
 
-export default function index({ data }) {
+export default function Index({ data }) {
+  const [searchedCountries, setSearchedCountries] = useState(null);
+  const [countries, setCountries] = useState(data ? data : null);
+  const [search, setSearch] = useState(null);
+
+  const searchCountries = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    if (countries && search) {
+      console.log(search);
+      setSearchedCountries(
+        countries.filter((array) => {
+          return array.name.common.includes(search);
+        })
+      );
+    }
+  }, [search]);
+
+  useEffect(() => {
+    if (searchedCountries) {
+      console.log(searchedCountries);
+      setCountries(searchedCountries);
+    }
+  }, [searchedCountries]);
+  useEffect(() => {
+      console.log(countries);
+  }, [countries]);
+
   return (
     <div className="countries-page">
-      <SearchBar />
-      <div className="total-of-results">
-        {`${data.length} items`}
-      </div>
-      <ul className="countries-list">
-        {data.map((item, _index) => (
-          <li name={`country${_index}`} className="card" key={_index}>
-            <div className="image-container">
-              {/*<img className="image" src={item.flags.svg} alt={`${item.name.common}' name`} />*/}
-            </div>
-            <div className="country-data">
-              <Link href={`/countries/${item.cca2.toLowerCase()}`}>
-                <a>
-                  <div className="country-name">
-                    <div className="code-name">
-                      <h2 className="country-code">{item.cca2}</h2>
-                      <span className="official-country-name">
-                        {item.name.common}
-                      </span>
-                    </div>
-                    <div>
-                      <span className="material-symbols-outlined icon">
-                        chevron_right
-                      </span>
-                    </div>
-                  </div>
-                </a>
-              </Link>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {countries ? (
+        <>
+          <div className="searchbar-container">
+            <div className="curve"></div>
+            <span className="material-symbols-outlined icon2">search</span>
+            <input
+              type={"text"}
+              placeholder={"Search by name"}
+              className="search-input"
+              onChange={searchCountries}
+            ></input>
+          </div>
+
+          <div className="total-of-results">{`${data.length} items`}</div>
+          <ul className="countries-list">
+            {data.map((item, _index) => (
+              <li name={`country${_index}`} className="card" key={_index}>
+                <div className="image-container">
+                  {/*<img className="image" src={item.flags.svg} alt={`${item.name.common}' name`} />*/}
+                </div>
+                <div className="country-data">
+                  <Link href={`/countries/${item.cca2.toLowerCase()}`}>
+                    <a>
+                      <div className="country-name">
+                        <div className="code-name">
+                          <h2 className="country-code">{item.cca2}</h2>
+                          <span className="official-country-name">
+                            {item.name.common}
+                          </span>
+                        </div>
+                        <div>
+                          <span className="material-symbols-outlined icon">
+                            chevron_right
+                          </span>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <div>Something went wrong :{"("} try again later</div>
+      )}
     </div>
   );
 }
